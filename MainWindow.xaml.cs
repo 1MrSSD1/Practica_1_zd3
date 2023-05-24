549ab65f3ce953979a35c42f918951e73655dee1
power <ruslan@gmail.com>
2023-05-24 11:41:31 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp74
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(txtC.Text);
+            double b = a * 9 / 5 + 32;
+            txtOtv.Content = $"°C = {a} " + $"°F = {b}";
+        }
+    }
+}
